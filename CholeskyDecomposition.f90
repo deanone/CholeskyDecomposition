253ac135428b14a7e93c9bb68bfabbc696ead26f
Athanasios Salamanis <asal@iti.gr>
2018-03-21 15:43:06 +0200

    Minor code refactoring.

--- CholeskyDecomposition.f90
+++ CholeskyDecomposition.f90
@@ -1,18 +1,21 @@
 program CholeskyDecomposition
 implicit none
+	! Define variables to be used
 	integer, parameter :: read_unit = 99
 	integer :: i, j, n, s, k
-	real :: start, finish
-	real, dimension(:, :), allocatable :: A
+	real :: start, finish,frobenius_norm
+	real, dimension(:, :), allocatable :: A, Anew
 	real, dimension(:, :), allocatable :: L
 	
 	print*, "Enter the size of the array:"
 	read*, n
+	
+	! Allocate memory for the matrices to be used
 	allocate(A(n, n))
 	allocate(L(n, n))
 	L ( : , : ) = 0.0
 	
-	! load the matrix from a .dat file
+	! Load the matrix from a .csv file
 	open(read_unit, file = 'matrix.csv', status = 'old')
 	do i = 1, n
 		read(read_unit, *) A(i, :)
@@ -40,13 +43,26 @@ implicit none
 	end do
 	call cpu_time(finish)
 	
-	deallocate(A)
+	! Compute Frobenius norm for the difference of A and L*L'
+	Anew = matmul(L, transpose(L))
+	frobenius_norm = 0;
+	do i = 1,n
+		do j = 1,n
+			frobenius_norm = frobenius_norm + abs(A(i,j) - Anew(i,j)) ** 2
+		end do 
+	end do
+	frobenius_norm = sqrt(frobenius_norm)
 	
+	! Print the L matrix and the Frobenius norm
 	print *, finish - start
 	! print the matrix
 	do i = 1, n
 		write(*, "(100g15.5)") (L(i, j), j = 1, n)
 	end do
 	
+	print *, frobenius_norm
+	
+	! Deallocate memory
+	deallocate(A)
 	deallocate(L)
 end program CholeskyDecomposition
